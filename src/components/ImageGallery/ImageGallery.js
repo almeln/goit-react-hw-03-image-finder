f20c3fd00dd755a2710fddd761ca456d294f1636
Alina Melnyk <alinkaaa345@gmail.com>
2021-09-14 00:06:36 +0300

    error message

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,9 +1,11 @@
 import { Component } from 'react';
+import ImageGalleryItem from 'components/ImageGalleryItem';
 
 class ImageGallery extends Component {
   state = {
     photos: null,
     loading: false,
+    error: null,
   };
 
   // Когда компонент обновляется (обновляются или пропсы или стейт)
@@ -21,22 +23,39 @@ class ImageGallery extends Component {
       fetch(
         `https://pixabay.com/api/?q=${nextName}&page=1&key=22659377-0dd97b237805bca735c774318&image_type=photo&orientation=horizontal&per_page=12`,
       )
-        .then(res => res.json())
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+
+          return Promise.reject(new Error(`No result with name ${nextName}`));
+        })
         .then(photos => this.setState({ photos: photos.hits }))
+        .catch(error => this.setState({ error }))
         .finally(() => this.setState({ loading: false }));
     }
   }
 
   render() {
-    const { photos, loading } = this.state;
+    const { photos, loading, error } = this.state;
     const { searchName } = this.props;
 
     return (
-      <ul className="ImageGallery">
+      <div>
+        {error && <h1>{error.message}</h1>}
         {loading && <div>Loading...</div>}
         {!searchName && <div>Введите критерий поиска</div>}
-        {photos && <div>{photos.id}</div>}
-      </ul>
+        {photos && (
+          <ul className="GalleryList">
+            {photos.map(photo => (
+              <li className="ImageGalleryItem" key={photo.id}>
+                <ImageGalleryItem src={photo.previewURL} alt={photo.tags} />
+              </li>
+            ))}
+          </ul>
+        )}
+        <p>{searchName}</p>
+      </div>
     );
   }
 }
