73d65f1bf49971f646d5d2a8487010f1493a9b68
Alina Melnyk <alinkaaa345@gmail.com>
2021-09-14 01:04:39 +0300

    adds using of state-machine

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -4,8 +4,9 @@ import ImageGalleryItem from 'components/ImageGalleryItem';
 class ImageGallery extends Component {
   state = {
     photos: null,
-    loading: false,
+    // loading: false,
     error: null,
+    status: 'idle',
   };
 
   // Когда компонент обновляется (обновляются или пропсы или стейт)
@@ -18,7 +19,7 @@ class ImageGallery extends Component {
       console.log('prevName', prevName);
       console.log('nextName', nextName);
 
-      this.setState({ loading: true, photos: null });
+      this.setState({ status: 'pending' });
 
       fetch(
         `https://pixabay.com/api/?q=${nextName}&page=1&key=22659377-0dd97b237805bca735c774318&image_type=photo&orientation=horizontal&per_page=12`,
@@ -30,33 +31,60 @@ class ImageGallery extends Component {
 
           return Promise.reject(new Error(`No result with name ${nextName}`));
         })
-        .then(photos => this.setState({ photos: photos.hits }))
-        .catch(error => this.setState({ error }))
-        .finally(() => this.setState({ loading: false }));
+        .then(photos =>
+          this.setState({ photos: photos.hits, status: 'resolved' }),
+        )
+        .catch(error => this.setState({ error, status: 'rejected' }));
+      // .finally(() => this.setState({ loading: false }));
     }
   }
 
   render() {
-    const { photos, loading, error } = this.state;
-    const { searchName } = this.props;
+    const { photos, error, status } = this.state;
+    // const { searchName } = this.props;
 
-    return (
-      <div>
-        {error && <h1>{error.message}</h1>}
-        {loading && <div>Loading...</div>}
-        {!searchName && <div>Введите критерий поиска</div>}
-        {photos && (
-          <ul className="GalleryList">
-            {photos.map(photo => (
-              <li className="ImageGalleryItem" key={photo.id}>
-                <ImageGalleryItem src={photo.previewURL} alt={photo.tags} />
-              </li>
-            ))}
-          </ul>
-        )}
-        <p>{searchName}</p>
-      </div>
-    );
+    // State-машина
+    if (status === 'idle') {
+      return <div>Enter search name</div>;
+    }
+
+    if (status === 'pending') {
+      return <div>Loading...</div>;
+    }
+
+    if (status === 'rejected') {
+      return <p>{error.message}</p>;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <ul className="GalleryList">
+          {photos.map(photo => (
+            <li className="ImageGalleryItem" key={photo.id}>
+              <ImageGalleryItem src={photo.previewURL} alt={photo.tags} />
+            </li>
+          ))}
+        </ul>
+      );
+    }
+
+    // return (
+    //   <div>
+    //     {error && <h1>{error.message}</h1>}
+    //     {loading && <div>Loading...</div>}
+    //     {!searchName && <div>Введите критерий поиска</div>}
+    //     {photos && (
+    //       <ul className="GalleryList">
+    //         {photos.map(photo => (
+    //           <li className="ImageGalleryItem" key={photo.id}>
+    //             <ImageGalleryItem src={photo.previewURL} alt={photo.tags} />
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     )}
+    //     <p>{searchName}</p>
+    //   </div>
+    // );
   }
 }
 
